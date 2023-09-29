205fe82b54d385a752298b189de30b7d4b5d4634
Henry Cahyo Nugroho <henry.smkn1kepanjen@gmail.com>
2023-09-29 21:51:28 +0700

    Fitur Search

--- lib/app/modules/all_todo/controllers/all_todo_controller.dart
+++ lib/app/modules/all_todo/controllers/all_todo_controller.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AllTodoController extends GetxController {
@@ -8,6 +9,8 @@ class AllTodoController extends GetxController {
   final count = 0.obs;
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  TextEditingController searchC = TextEditingController();
 
   @override
   void onInit() {
@@ -28,15 +31,30 @@ class AllTodoController extends GetxController {
 
   Future<QuerySnapshot<Map<String, dynamic>>> getAllResult() async {
     String uid = auth.currentUser!.uid;
-    QuerySnapshot<Map<String, dynamic>> query = await firestore
-        .collection("users")
-        .doc(uid)
-        .collection("todos")
-        .orderBy(
-          "created_at",
-          descending: true,
-        )
-        .get();
+    QuerySnapshot<Map<String, dynamic>> query;
+    if (searchC.text != "") {
+      query = await firestore
+          .collection("users")
+          .doc(uid)
+          .collection("todos")
+          .where("namadudi", isGreaterThanOrEqualTo: searchC.text)
+          .where("namadudi", isLessThan: "${searchC.text}z")
+          // .orderBy(
+          //   "created_at",
+          //   descending: true,
+          // )
+          .get();
+    } else {
+      query = await firestore
+          .collection("users")
+          .doc(uid)
+          .collection("todos")
+          .orderBy(
+            "created_at",
+            descending: true,
+          )
+          .get();
+    }
     return query;
   }
 }
